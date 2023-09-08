95ca19eab925bf60c095a779302f8129064a4387
JeniferDuran <144250210+JeniferDuran@users.noreply.github.com>
2023-09-08 10:04:57 -0600

    Subida de Fichero

---
+++ lib/pages/alejandra.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class alejandra extends StatelessWidget {
+  const alejandra({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
