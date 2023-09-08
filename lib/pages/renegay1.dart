84abcb0a7627f325b53d648f9371c36f6a0f85c7
MAFRAlexan <idalia.grande22@itca.edu.sv>
2023-09-08 11:38:16 -0600

    idalia

---
+++ lib/pages/renegay1.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class renegay extends StatelessWidget {
+  const renegay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
