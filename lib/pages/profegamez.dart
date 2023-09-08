55c554eeb1e9e464a0405ed69be1eb089ce7d5df
devgamez2022 <101201095+devgamez2022@users.noreply.github.com>
2023-09-08 09:53:02 -0600

    adicion del fichero profgamez dart

---
+++ lib/pages/profegamez.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Profgamez extends StatelessWidget {
+  const Profgamez({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
