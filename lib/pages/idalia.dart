e61ecc1cc6a1dba1493468fa228416f4edc7d9db
MAFRAlexan <idalia.grande22@itca.edu.sv>
2023-09-08 11:26:01 -0600

    nuevo aporte

---
+++ lib/pages/idalia.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class idalia extends StatelessWidget {
+  const idalia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
