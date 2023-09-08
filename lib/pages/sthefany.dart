473ba5e11b4b41e3fcbf6dd193cb839cb962b254
MariaBernal23 <144250332+MariaBernal23@users.noreply.github.com>
2023-09-08 10:03:46 -0600

    sthefany

---
+++ lib/pages/sthefany.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class sthefany extends StatefulWidget {
+  const sthefany({super.key});
+
+  @override
+  State<sthefany> createState() => _sthefanyState();
+}
+
+class _sthefanyState extends State<sthefany> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
