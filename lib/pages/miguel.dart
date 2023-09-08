0c15787e292ef72483d16e39bb3ca2e68ea7d74f
SCP-Antonio <scpantonio01@gmail.com>
2023-09-08 10:53:42 -0600

    miguel

---
+++ lib/pages/miguel.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class miguel extends StatelessWidget {
+  const miguel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
