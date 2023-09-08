9b0814ea66d9230cc152a9ab4fc42bee4df613c9
yarethzy <rl444705@gmail.com>
2023-09-08 11:26:53 -0600

    subida 123

--- lib/pages/renehd.dart
+++ lib/pages/renehd.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class reneHD extends StatelessWidget {
   const reneHD({super.key});
-//actualizacion
+
+//actualizacion 1
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
