e7cf6742b6da10e3f4fab64d64d4c77e802c85b8
Douglas-prog23 <131540385+Douglas-prog23@users.noreply.github.com>
2023-09-08 10:40:48 -0600

    fichero fgdgdg

--- lib/pages/cecilia.dart
+++ lib/pages/cecilia.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class Cecilia extends StatelessWidget {
-  const Cecilia({super.key});
+
+class cecilia extends StatelessWidget {
+  const cecilia({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
-
