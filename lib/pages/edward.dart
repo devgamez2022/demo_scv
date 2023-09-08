36db99ffcc878581ae4dcc12b8e5b781c762671a
Cesar-zelaya <139627259+Cesar-zelaya@users.noreply.github.com>
2023-09-08 10:09:30 -0600

    lo subio edward

--- lib/pages/edward.dart
+++ lib/pages/edward.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dars'
-    
-    class edward  extends StatelessWidget {
-      const ({super.key});
-    
+import 'package:flutter/material.dart' ;
+
+    class edward extends StatelessWidget {
+      const edward ({super.key});
+
       @override
       Widget build(BuildContext context) {
         return const Placeholder();
