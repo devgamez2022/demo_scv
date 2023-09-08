cc1a69e32a3fc2bb0af570e0f83f621fd1bae1f7
HaydeeGrande <143965227+HaydeeGrande@users.noreply.github.com>
2023-09-08 10:03:32 -0600

    yordy

---
+++ lib/pages/yordyhd.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class yordyhd extends StatelessWidget {
+  const yordyhd({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
