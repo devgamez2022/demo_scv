1717c88248c3d48f4cf86dc7bc678bfd4ec94adc
adonay2003 <108413232+adonay2003@users.noreply.github.com>
2023-09-08 10:45:40 -0600

    adicion del fichero carolinaa

---
+++ lib/pages/carolinaa.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class carolinaa extends StatelessWidget {
+  const carolinaa({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
